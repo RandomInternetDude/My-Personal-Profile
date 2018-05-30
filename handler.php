66026bb40f84f90634636be2ff1b389e6ca61d23
Joey Rivera <joey7040@hotmail.com>
2018-05-29 20:53:21 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+$pp->sendEmailTo('joey7040@hotmail.com'); 
 
 echo $pp->process($_POST);
